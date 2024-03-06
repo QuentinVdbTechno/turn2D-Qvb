2702a9032eb08c3e06454a23d0b94d8f04973d61
QuentinVdbTechno <techno.quentin.vdb@gmail.com>
2024-03-06 10:54:53 +0100

    Tuto Fini
    la grille fonctionne, on peut déplacer le personnage et détruire l'ennemi

--- Assets/Scripts/Tiles/GrassTile.cs
+++ Assets/Scripts/Tiles/GrassTile.cs
@@ -2,15 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GrassTile :  Tile
+public class GrassTile : Tile
 {
-
     [SerializeField] private Color _baseColor, _offsetColor;
 
-    public override void Init(int x, int y)
-    {
+    public override void Init(int x, int y) {
         var isOffset = (x + y) % 2 == 1;
         _renderer.color = isOffset ? _offsetColor : _baseColor;
     }
-
 }
